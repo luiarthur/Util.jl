5e26e4c5347427c3da487de4f2a4193df3c9e5c8
Arthur Lui <luiarthur@gmail.com>
2020-12-06 23:00:05 -0700

    added documentation

--- src/io.jl
+++ src/io.jl
@@ -1,5 +1,20 @@
 """
 Redirects stdout to a file.
+
+# Example
+
+```julia
+import Util
+
+Util.redirect_stdout_to_file("log.txt") do
+  for i in 1:20
+    sleep(.1)
+    print("#")
+    flush(stdout)
+  end
+  println()
+end
+```
 """
 function redirect_stdout_to_file(f::Function, path::String)
   open(path, "w") do io
--- src/s3.jl
+++ src/s3.jl
@@ -1,1 +1,11 @@
+"""
+Equivalent to `aws s3 sync <from> <to> <tags>` in terminal.
+
+# Example
+
+```julia
+import Util
+Util.s3sync(from="resultsdir", to="s3://my-bucket", tags=`--exclude '*.nfs'`)
+```
+"""
 s3sync(; from, to, tags=``) = run(`aws s3 sync $(from) $(to) $(tags)`)
